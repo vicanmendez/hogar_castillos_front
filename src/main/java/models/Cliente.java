f577001b0b51d6a0c5b2a5b3f4d5661bdd033507
VICTOR ANDRÉS MÉNDEZ LOPEZ <46352621@ceibal.edu.uy>
2024-12-09 19:27:02 -0300

    Primera versión con residentes y socios funcionando. Falta la generación de recibos y mejorar la UI

---
+++ src/main/java/models/Cliente.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Víctor
+ */
+public class Cliente {
+    private String id;
+    private String ci;
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Cliente(String id, String ci, String nombre, String telefono, String email) {
+        this.id = id;
+        this.ci = ci;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+    
+    
+    
+    
+}
